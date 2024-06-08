b11163200879f3739b2c44ab48f9043344a2dc93
masudmolla6 <masudmolla2937@gmail.com>
2024-06-08 09:58:11 +0600

    added data

--- index.js
+++ index.js
@@ -24,7 +24,50 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-  try {
+    try {
+      
+        const database = client.db("reviveDB");
+        const usedProducts = database.collection("usedProducts");
+        const products = database.collection("products");
+        const bookings = database.collection("bookings");
+
+        app.get("/usedProducts", async (req, res) => {
+          const result = await usedProducts.find().toArray();
+          res.send(result);
+        });
+
+        app.get("/usedProducts/:id", async (req, res) => {
+          const id = req.params.id;
+
+          console.log(id);
+
+          const allProducts = await products.find().toArray();
+
+          const specificProducts = allProducts.filter(
+            (products) => parseInt(products.category) === parseInt(id)
+          );
+
+          console.log(specificProducts);
+
+          res.send(specificProducts);
+        });
+
+        app.get("/bookings", async (req, res) => {
+          const result = await bookings.find().toArray();
+
+          res.send(result);
+        });
+
+        app.post("/bookings", async (req, res) => {
+          const newProduct = req.body;
+
+          console.log(newProduct);
+
+          const result = await bookings.insertOne(newProduct);
+
+          res.send(result);
+        });
+
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
